b07e5e517a8cfb6f1e017e5d262142ee85e9fabb
IvarKristinn <hallssonivarkristinn@gmail.com>
2018-09-25 16:32:57 +0000

    Constructor for ModelFormatExp and ResourceNotFoundExp

--- Exterminator.Models/Exceptions/ModelFormatException.cs
+++ Exterminator.Models/Exceptions/ModelFormatException.cs
@@ -4,6 +4,8 @@ namespace Exterminator.Models.Exceptions
 {
     public class ModelFormatException : Exception
     {
-        
+        public ModelFormatException() : base("Model is not properly formatted") {}
+        public ModelFormatException(string message) : base(message) {}
+        public ModelFormatException(string message, Exception inner) : base(message, inner) {}
     }
 }
