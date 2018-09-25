8e29fea4feca10349aa799c3d998a4d5a9567c25
villi <vilhjalmur12@ru.is>
2018-09-25 15:09:40 +0000

    log to database

--- Exterminator.WebApi/ExceptionHandlerExtensions/ExceptionHandlerExtensions.cs
+++ Exterminator.WebApi/ExceptionHandlerExtensions/ExceptionHandlerExtensions.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Exterminator.Models;
 using Exterminator.Models.Exceptions;
 using Exterminator.Services.Interfaces;
+using Exterminator.Services.Implementations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
@@ -20,21 +21,39 @@ namespace Exterminator.WebApi.ExceptionHandlerExtensions
                 error.Run(async context => 
                 {
                     var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    
+
                     if(exceptionHandlerFeature != null) {
                         var exception = exceptionHandlerFeature.Error;
                         var statusCode = (int) HttpStatusCode.InternalServerError;
                         context.Response.ContentType = "application/json";
 
-                        if(exception is ArgumentOutOfRangeException) {
+                        if(exception is ResourceNotFoundException) {
+                            statusCode = (int) HttpStatusCode.NotFound;
+                        } else if (exception is ModelFormatException) {
+                            statusCode = (int) HttpStatusCode.PreconditionFailed;
+                        } else if (exception is ArgumentOutOfRangeException) {
                             statusCode = (int) HttpStatusCode.BadRequest;
-                        } 
+                        }
                         // TODO: setja fleiri exception í if setningu hér
+
+                        ExceptionModel exceptionModel = new ExceptionModel{
+                            StatusCode = statusCode,
+                            ExceptionMessage = exception.Message,
+                            StackTrace = exception.StackTrace.ToString
+                        };
+
+                        var logService = app.ApplicationServices.GetService(typeof(ILogService)) as ILogService;
+                        logService.LogToDatabase(exceptionModel);
 
                         await context.Response.WriteAsync(new ExceptionModel{
                             StatusCode = statusCode,
                             ExceptionMessage = exception.Message,
                             StackTrace = exception.StackTrace.ToString
                         }.ToString);
+
+                        
                     }
                 });
             });
