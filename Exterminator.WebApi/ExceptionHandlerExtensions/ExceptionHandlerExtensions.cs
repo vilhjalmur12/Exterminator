b5e8bb3bbed9795542ee3f1bd99c53df19317f98
villi <vilhjalmur12@ru.is>
2018-09-25 14:44:36 +0000

    ExceptionHandlerExtension

--- Exterminator.WebApi/ExceptionHandlerExtensions/ExceptionHandlerExtensions.cs
+++ Exterminator.WebApi/ExceptionHandlerExtensions/ExceptionHandlerExtensions.cs
@@ -15,6 +15,29 @@ namespace Exterminator.WebApi.ExceptionHandlerExtensions
         public static void UseGlobalExceptionHandler(this IApplicationBuilder app)
         {
             // TODO: Implement
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context => 
+                {
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if(exceptionHandlerFeature != null) {
+                        var exception = exceptionHandlerFeature.Error;
+                        var statusCode = (int) HttpStatusCode.InternalServerError;
+                        context.Response.ContentType = "application/json";
+
+                        if(exception is ArgumentOutOfRangeException) {
+                            statusCode = (int) HttpStatusCode.BadRequest;
+                        } 
+                        // TODO: setja fleiri exception í if setningu hér
+
+                        await context.Response.WriteAsync(new ExceptionModel{
+                            StatusCode = statusCode,
+                            ExceptionMessage = exception.Message,
+                            StackTrace = exception.StackTrace.ToString
+                        }.ToString);
+                    }
+                });
+            });
         }
     }
 }
